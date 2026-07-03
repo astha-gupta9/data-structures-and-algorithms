8b6b5993f87a384ed77ef3cac688313df8e0d1b9
Astha Gupta <aasthaa0024@gmail.com>
2026-07-03 21:02:39 +0530

    Solve LC 451 - Sort Characters By Frequency using Bucket Sort

--- Strings/SortCharactersByFrequency.java
+++ Strings/SortCharactersByFrequency.java
@@ -1,8 +1,5 @@
 // Leetcode 451 - Sort Characters By Frequency
 
-// Time complexity: O(nlogn)
-// Space complexity: O(n)
-
 package Strings;
 
 import java.util.ArrayList;
@@ -11,7 +8,10 @@ import java.util.List;
 import java.util.Map;
 
 public class SortCharactersByFrequency {
-    public String frequencySort(String s) {
+    
+    // Time complexity: O(nlogn)
+    // Space complexity: O(n)
+    public String comparisonSort(String s) {
         
         // frequency count of each character of string
         Map<Character, Integer> charMap = new HashMap<>();
@@ -31,6 +31,49 @@ public class SortCharactersByFrequency {
             while (freq > 0) {
                 result.append(charList.get(i).getKey());
                 freq--;
+            }
+        }
+
+        return result.toString();
+    }
+
+    // Time complexity: O(n)
+    // Space complexity: O(n)
+    public String frequencySort(String s) {
+
+        // Bucket Sort
+
+        // count frequency of each char of string
+        Map<Character, Integer> charMap = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            charMap.put(ch, charMap.getOrDefault(ch, 0) + 1);
+        }
+
+        // bucket chars with same frequency
+        List<Character>[] bucket = new ArrayList[s.length() + 1];
+        for (Map.Entry<Character, Integer> e : charMap.entrySet()) {
+            int freq = e.getValue();
+
+            if (bucket[freq] == null) {
+                bucket[freq] = new ArrayList<Character>();
+            }
+            bucket[freq].add(e.getKey());
+        }
+
+        // generate result string
+        StringBuilder result = new StringBuilder();
+        for (int i = bucket.length - 1; i > 0; i--) {
+
+            if (bucket[i] == null)
+                continue;
+
+            // traverse bucket for current frequency
+            for (char ch : bucket[i]) {
+
+                // append each char i number of times in result string
+                for (int j = 0; j < i; j++) {
+                    result.append(ch);
+                }
             }
         }
 
